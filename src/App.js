f504f45e5a4cb688f003fdc02bbc48da2e6931e2
Aamir <aaimrmakofficial@gmail.com>
2021-10-24 23:14:07 +0530

    2nd Commit VS

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ function App() {
   const [count, setcount] = useState(0)
 return (
 <>
+<h1>Putting a Change</h1>
 <button onClick={() => setcount(count-1)} >-</button>
 <span>{count}</span>
 <button onClick={() => setcount(count+1)} >+</button>
